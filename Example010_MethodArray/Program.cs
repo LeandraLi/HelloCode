330d2207d6805cc7f733a06390fb93e816e13f9b
LeandraLi <leandra2005@yandex.ru>
2023-02-12 13:30:25 +0300

    добавлен пример 10 с лекции 2

--- Example010_MethodArray/Program.cs
+++ Example010_MethodArray/Program.cs
@@ -1,3 +1,18 @@
-﻿int[] array = { 1, 12, 31, 4, 15, 16, 17, 18 };
+﻿int[] array = { 1, 12, 31, 4, 15, 18, 16, 17, 18 };
 
+int n = array.Length;
+int find = 18;
 
+int index = 0;
+
+while (index < n)
+{
+    
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break; // добавляем прерывание операции после нахождения первого элемента, соответствующего условию - если одинаковых значений в массиве несколько
+    }
+    // index = index +1;
+    index++;
+}
