e5b2338cc7ae9b52d887614196ff22410c4032ae
LeandraLi <leandra2005@yandex.ru>
2023-02-25 09:52:27 +0300

    оптимизирована задача 4 из лекции 1

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,3 @@
-﻿double numberA = 12;
+﻿double numberA = 12; // int - integer - целые числа, не подходят, если мы хотим получить правильное решение
 double numberB = 5;
 Console.WriteLine(numberA / numberB);
